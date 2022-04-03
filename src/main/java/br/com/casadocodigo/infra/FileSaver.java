7bf132b31a95d3881dc514f0080da8f9532e2be3
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-04-03 12:40:07 -0300

    Recuperando a imagem para a página

--- src/main/java/br/com/casadocodigo/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/infra/FileSaver.java
@@ -1,21 +1,54 @@
 package br.com.casadocodigo.infra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Path;
 
 import javax.servlet.http.Part;
 
 public class FileSaver {
 
-	private static final String SERVER_PATH = "D:\\eclipse-workspace\\casadocodigo\\src\\main\\resources";
+	public static final String SERVER_PATH = "D:\\eclipse-workspace\\casadocodigo\\src\\main\\resources";
 
 	public String write(Part arquivo, String path) {
-		String relativePath = path + "\\" + arquivo.getSubmittedFileName();
+		String relativePathServ = path + "\\" + arquivo.getSubmittedFileName();
+		String relativePathReturn = path + "/" + arquivo.getSubmittedFileName();
 		try {
-			arquivo.write(SERVER_PATH + "\\" + relativePath);
-			return relativePath;
+			arquivo.write(SERVER_PATH + "\\" + relativePathServ);
+			return relativePathReturn;
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static void transfer(Path source, OutputStream outputStream) {
+		try {
+			FileInputStream input = new FileInputStream(source.toFile());
+			try (ReadableByteChannel inputChannel = Channels.newChannel(input);
+					WritableByteChannel outputChannel = Channels.newChannel(outputStream)) {
+				
+//				Fazendo um buffer de 10 KB - O arquivo será quebrado em pacotes de 10 KB para tranferência.
+				ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 10);
+
+				while (inputChannel.read(buffer) != -1) {
+					buffer.flip();
+					outputChannel.write(buffer);
+					buffer.clear();
+				}
+
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
 }
